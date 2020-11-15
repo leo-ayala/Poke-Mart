62aebb542674613cfe1763f32f82ac5d164cbe0d
Leo Ayala <leo_a1016@yahoo.com>
2020-11-15 14:18:07 -0500

    mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB || 'mongodb://localhost/mernleozon', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernleozon', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
